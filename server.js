164e2120d092ecb9cc4786958ced4c593745c0d5
saidgeek <atarix.010101@gmail.com>
2012-01-23 21:06:53 -0300

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var fs = require("fs"),
+	http = require("http");
+
+// servidor
+server = http.createServer(function(reg, res)
+{
+	fs.readFile("index.html", function(err, data){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(data);
+		res.end();
+	});
+});
+
+server.listen(8080);
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+everyone.now.distributeMessage = function(msg){
+	everyone.now.receiveMessage(this.now.name, msg);
+}
